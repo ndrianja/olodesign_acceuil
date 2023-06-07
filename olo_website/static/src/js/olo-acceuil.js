6b56f3ff5fa3eb40a17128d1ad6f716d45dab783
AinaFanambinantsoa <fanambinantsoaina@gmail.com>
2023-06-07 11:14:41 +0300

    ajout de page candidat
    juste le template indépendant

--- olo_website/static/src/js/olo-acceuil.js
+++ olo_website/static/src/js/olo-acceuil.js
@@ -12,4 +12,24 @@ window.onload = function () {
         document.querySelector('i.bx-chevron-up').classList.add('d-none');
         document.querySelector('i.bx-menu').classList.remove('d-none');
     }
+
+
+    let valueDisplays = document.querySelectorAll(".num");
+    let interval = 2000;
+
+    console.log(valueDisplays);
+
+    valueDisplays.forEach((valueDisplay) => {
+        let startValue = 0;
+        let endValue = parseInt(valueDisplay.getAttribute("data-val"));
+
+        let duration = Math.floor(interval / endValue);
+        let counter = setInterval(function () {
+            startValue += 1;
+            valueDisplay.textContent = startValue;
+            if (startValue == endValue) {
+                clearInterval(counter);
+            }
+        }, duration);
+    });
 }
